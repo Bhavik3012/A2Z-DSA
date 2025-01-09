c0e5fa29a6392a18149d84d91f1abce2ffdd225d
Bhavik3012 <bhaviksongara301205@gmail.com>
2025-01-09 22:11:17 +0530

    bubblesort.cpp

--- 03_sorting/sorting.cpp
+++ 03_sorting/sorting.cpp
@@ -19,6 +19,29 @@ void selection_sort(int arr[], int n)
         arr[i] = temp;
     }
 }
+
+void bubble_sort(int arr[], int n)
+{
+    for (int i = n - 1; i > 0; i--)
+    {
+        int didSwap = 0;
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                didSwap++;
+                cout << "swap";
+            }
+        }
+        if (didSwap == 0)
+        {
+            break;
+        }
+    }
+}
 int main()
 {
     int n;
@@ -28,7 +51,8 @@ int main()
     {
         cin >> arr[i];
     }
-    selection_sort(arr, n);
+    // selection_sort(arr, n);
+    bubble_sort(arr, n);
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
