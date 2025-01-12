67e361433d639780c5f4cfbdc3417dbcc0905c7b
Bhavik3012 <bhaviksongara301205@gmail.com>
2025-01-12 18:30:52 +0530

    leetcode 2166

---
+++ 01_LeetCode/2166_check_if_parenthessis_string_can_valid.cpp
@@ -0,0 +1,47 @@
+class Solution
+{
+public:
+    bool canBeValid(string s, string locked)
+    {
+        int n = s.length();
+        if (n % 2 != 0)
+        {
+            return false;
+        }
+
+        int opencount = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '(' || locked[i] == '0')
+            {
+                opencount++;
+            }
+            else
+            {
+                opencount--;
+            }
+            if (opencount < 0)
+            {
+                return false;
+            }
+        }
+
+        int closecount = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (s[i] == ')' || locked[i] == '0')
+            {
+                closecount++;
+            }
+            else
+            {
+                closecount--;
+            }
+            if (closecount < 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
